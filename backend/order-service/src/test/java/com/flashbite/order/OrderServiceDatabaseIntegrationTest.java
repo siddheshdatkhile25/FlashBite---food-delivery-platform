b740770b2c70e4f298440330c9cb00492d2b9d33
Siddhesh Datkhile <siddhesh.datkhile@joshsoftware.com>
2026-08-11 12:05:17 +0530

    chore: disable Testcontainers without Docker and remove redundant flyway-database-postgresql dependencies

--- backend/order-service/src/test/java/com/flashbite/order/OrderServiceDatabaseIntegrationTest.java
+++ backend/order-service/src/test/java/com/flashbite/order/OrderServiceDatabaseIntegrationTest.java
@@ -12,7 +12,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class OrderServiceDatabaseIntegrationTest {
 
